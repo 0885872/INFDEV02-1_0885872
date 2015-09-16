bb6e79e2a461bdf48c4e489945802aa884f03628
Timmy <timmie@zeelandnet.nl>
2015-09-16 09:15:23 +0200

    Asterisk_first

---
+++ Asterisk_Python/Asterisk_Python/Asterisk_Python.py
@@ -0,0 +1,22 @@
+"""def asterisk_cheating():
+    print ("**********")
+    print ("********* ")
+    print ("********  ")
+    print ("*******   ")
+    print ("******    ")
+    print ("*****     ")
+    print ("****      ")
+    print ("***       ")
+    print ("**        ")
+    print ("*         ")
+
+asterisk_cheating()"""
+
+def asterisk():
+    x = 0
+    while x != 10:
+        print ("*" * x)
+        x+=1
+
+
+asterisk()
--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -5,25 +5,3 @@
 
 greeting()
 
-def asterisk_cheating():
-    print ("**********")
-    print ("********* ")
-    print ("********  ")
-    print ("*******   ")
-    print ("******    ")
-    print ("*****     ")
-    print ("****      ")
-    print ("***       ")
-    print ("**        ")
-    print ("*         ")
-
-asterisk_cheating()
-
-def asterisk():
-    x = 0
-    while x != 10:
-        print ("*" * x)
-        x+=1
-
-
-asterisk()
