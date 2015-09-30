5477f95a8b67a40b9744c81fdafd4473ec81e268
Timmy <timmie@zeelandnet.nl>
2015-09-30 11:17:43 +0200

    Cryptography added

---
+++ Cryptography/Cryptography/Cryptography.py
@@ -0,0 +1,14 @@
+﻿word = raw_input("Please insert your words: ")
+shiftedAmount = int(raw_input("Insert shifted amount: "))
+if word.isdigit(): 
+    print "invalid entry"
+else:
+    for x in range (0,len(word)):
+        cryptoDigit = ord(word[x]) + shiftedAmount
+        cryptoLetter = chr(cryptoDigit)
+        print cryptoLetter
+        
+
+
+          
+
--- passwordCheck/passwordCheck/passwordCheck.py
+++ passwordCheck/passwordCheck/passwordCheck.py
@@ -6,6 +6,8 @@ def passwordCheck():
         print "Password is weak"
     elif len(password) > 10:
         print "Password contains 10+ characters, password is strong."
+    elif (password.isupper or password.isspace or password.isdigit) and set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
+        print "Password contains uppercase, space or digits AND special characters. Password is strong."
 
         """create a set containing all your invalid characters, then take the intersection of it and password (in other words, a set containing all unique characters that exist in both the set and the password string). 
         If there are no matches, the resulting set will be empty, and thus evaluate as False, otherwise it will evaluate as True and print the message."""
@@ -14,8 +16,7 @@ def passwordCheck():
         print "Password contains special characters. Password is medium"
     elif password.isupper or password.isspace or password.isdigit:
         print "Password contains uppercase, space or digits. Password is medium."
-    elif (password.isupper or password.isspace or password.isdigit) and set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
-        print "Password contains uppercase, space or digits AND special characters. Password is strong."
+   
    
     
 
