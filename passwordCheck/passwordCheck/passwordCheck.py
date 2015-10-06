4c8093fbd57be1d113b17aad6108bc4a0ff0b830
Timmy <timmie@zeelandnet.nl>
2015-10-06 21:43:34 +0200

    version 2.1

--- passwordCheck/passwordCheck/passwordCheck.py
+++ passwordCheck/passwordCheck/passwordCheck.py
@@ -1,24 +1,22 @@
 ﻿import re
 
-def passwordCheck():
-    password = raw_input("Please insert your password ")
-    if len(password) < 3:
-        print "Password is weak"
-    elif len(password) > 10:
-        print "Password contains 10+ characters, password is strong."
-    elif (password.isupper or password.isspace or password.isdigit) and set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
-        print "Password contains uppercase, space or digits AND special characters. Password is strong."
+password = raw_input("Please insert your password ")
+if len(password) < 3:
+    print "Password is weak"
+elif len(password) > 10:
+    print "Password contains 10+ characters, password is strong."
+elif (password.isupper or password.isspace or password.isdigit) and password == ('[~!@#$%^&*()_+{}":;\']+$'):
+    print "Password contains uppercase, space or digits AND special characters. Password is strong."
 
-        """create a set containing all your invalid characters, then take the intersection of it and password (in other words, a set containing all unique characters that exist in both the set and the password string). 
-        If there are no matches, the resulting set will be empty, and thus evaluate as False, otherwise it will evaluate as True and print the message."""
+        #"""create a set containing all your invalid characters, then take the intersection of it and password (in other words, a set containing all unique characters that exist in both the set and the password string). 
+        #If there are no matches, the resulting set will be empty, and thus evaluate as False, otherwise it will evaluate as True and print the message."""
 
-    elif set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
-        print "Password contains special characters. Password is medium"
-    elif password.isupper or password.isspace or password.isdigit:
-        print "Password contains uppercase, space or digits. Password is medium."
+elif password == ('[~!@#$%^&*()_+{}":;\']+$'):
+    print "Password contains special characters. Password is medium"
+elif password.isupper or password.isspace or password.isdigit:
+    print "Password contains uppercase, space or digits. Password is medium."
    
    
     
 
 
-passwordCheck()
